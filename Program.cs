b10c139d1efc52eb9258f2c2b461073d9e889ba7
7Games <benblack656@gmail.com>
2021-12-12 18:10:12 +0000

    Updated Program.cs ¬ Made cursor and buttons more compress. Changed some characters. And added comments. 310 to 189 lines

--- Program.cs
+++ Program.cs
@@ -1,310 +1,189 @@
-﻿using System;
+﻿/*	
+	C# Calculator ¬ 7Games
+	Created: 11/12/21
+	Last Edited: 12/12/21
+*/
+
+using System;
 
 namespace C__Calculator {
-    class Program {
-        
-        static bool running = true;
-        static int cursorPosition = 5;
-        static char cursorChar = '#';
-        static string result = "";
+	class Program {
+		
+		//Ints
+		static int[] buttonLocations = {43, 46, 49, 52, 71, 74, 77, 80, 99, 102, 105, 108, 127, 130, 133, 136};
+		static int cursorPosition = 5;
+		static int calcOperator = 0;
+		static int numberOn = 0;
+		//Doubles
+		static double firstNumber = 0;
+		static double secondNumber = 0;
+		
+		//Chars
+		static char cursorChar = '#';
+		//Strings
+		static string result = "";
+		static string calculator = "";
+		static string currentNumber = "";
+		
+		//Bools
+		static bool running = true;
 
-        static double firstNumber = 0;
-        static double secondNumber = 0;
-        static int numberOn = 0;
-        static string currentNumber = "";
-        static int thing = 0;
+		static void drawCalaulator() {
+			//Converts the calculator string to a char array to make it easier to edit.
+			char[] charCalculator = calculator.ToCharArray();
 
-        static void drawCalaulator() {
-            string calculator = "╔═══════════╗\n║...........║\n╠══╦══╦══╦══╣\n║ 1║ 2║ 3║ +║\n╠══╬══╬══╬══╣\n║ 4║ 5║ 6║ -║\n╠══╬══╬══╬══╣\n║ 7║ 8║ 9║ *║\n╠══╬══╬══╬══╣\n║ C║ 0║ =║ ÷║\n╚══╩══╩══╩══╝";
-            char[] charCalculator = calculator.ToCharArray();
+			//Makes sure that the cursor is in a acceptable location.
+			switch(cursorPosition) {
+				//Out of bounds
+				case <0:
+					charCalculator[43] = cursorChar;
+					cursorPosition = 0;
+					break;
+				case >15:
+					charCalculator[136] = cursorChar;
+					cursorPosition = 15;
+					break;
+			}
 
-            switch(cursorPosition) {
-                //First Button Row
-                case 0:
-                    charCalculator[43] = cursorChar;
-                    break;
-                case 1:
-                    charCalculator[46] = cursorChar;
-                    break;
-                case 2:
-                    charCalculator[49] = cursorChar;
-                    break;
-                case 3:
-                    charCalculator[52] = cursorChar;
-                    break;
-                //Second Button Row
-                case 4:
-                    charCalculator[71] = cursorChar;
-                    break;
-                case 5:
-                    charCalculator[74] = cursorChar;
-                    break;
-                case 6:
-                    charCalculator[77] = cursorChar;
-                    break;
-                case 7:
-                    charCalculator[80] = cursorChar;
-                    break;
-                //Third Button Row
-                case 8:
-                    charCalculator[99] = cursorChar;
-                    break;
-                case 9:
-                    charCalculator[102] = cursorChar;
-                    break;
-                case 10:
-                    charCalculator[105] = cursorChar;
-                    break;
-                case 11:
-                    charCalculator[108] = cursorChar;
-                    break;
-                //Forth Button Row
-                case 12:
-                    charCalculator[127] = cursorChar;
-                    break;
-                case 13:
-                    charCalculator[130] = cursorChar;
-                    break;
-                case 14:
-                    charCalculator[133] = cursorChar;
-                    break;
-                case 15:
-                    charCalculator[136] = cursorChar;
-                    break;
-                //Out of bounds
-                case <0:
-                    charCalculator[43] = cursorChar;
-                    cursorPosition = 0;
-                    break;
-                case >15:
-                    charCalculator[136] = cursorChar;
-                    cursorPosition = 15;
-                    break;
-            }
-            
-            calculator = new string(charCalculator);
+			//Moves the cursor to the button location.
+			charCalculator[buttonLocations[cursorPosition]] = cursorChar;
+			
+			calculator = new string(charCalculator);
 
-            string dots = "";
+			//Makes dots appear before the characters in the result box.
+			string dots = "";
+			for (int i = 0; i<(11 - result.ToCharArray().Length); i++){
+				dots += "·";
+			}
 
-            for (int i = 0; i<(11 - result.ToCharArray().Length); i++){
-                dots += "·";
-            }
+			/*Check to see if the length of result is bigger than 11, 
+			if it is it makes the string 9 characters then adds two dots to the begining of the string.*/
+			if(result.Length>11){
+				result = result.Substring(result.Length - 9, 9);
+				result = result.Insert(0, "..");
+			}
+			
+			calculator = calculator.Replace("···········", (dots + result));
+			
+			Console.WriteLine(calculator);
+		}
 
-            if(result.Length>11){
-                result = result.Substring(result.Length - 9, 9);
-                result = result.Insert(0, "..");
-            }
-            calculator = calculator.Replace("...........", (dots + result));
-            
+		static void input() {
+			switch(Console.ReadKey().Key){
+				//Moves the cursor.
+				case ConsoleKey.UpArrow:
+					cursorPosition-=4;
+					break;
+				case ConsoleKey.DownArrow:
+					cursorPosition+=4;
+					break;
+				case ConsoleKey.RightArrow:
+					cursorPosition+=1;
+					break;
+				case ConsoleKey.LeftArrow:
+					cursorPosition-=1;
+					break;
+				//Selects the button the cursor is currently on.
+				case ConsoleKey.Spacebar:
+					//Numbers
+					if(cursorPosition != 3 && cursorPosition != 7 && cursorPosition != 11 && cursorPosition != 12 && cursorPosition != 14 && cursorPosition != 15){
+						if (numberOn < 2) {
+							//Adds the number currently selected to the result box.
+							char[] charCalculator = calculator.ToCharArray();
+							result += charCalculator[buttonLocations[cursorPosition] + 1];
+							currentNumber += charCalculator[buttonLocations[cursorPosition] + 1];
+						}
+					}
 
-            Console.WriteLine(calculator);
-        }
+					//Calculator Operators
+					if(cursorPosition == 3 || cursorPosition == 7 || cursorPosition == 11 || cursorPosition == 15){
+						if (numberOn == 0) {
+							int[] operatorLocation = {3, 7, 11, 15};
+							char[] charCalculator = calculator.ToCharArray();
+							result += charCalculator[buttonLocations[cursorPosition] + 1];
+							
+							for(int i = 0; i < 3; i++) {
+								if (cursorPosition - operatorLocation[i] == 0) {
+									calcOperator = i;
+								}
+							}
 
-        static void input() {
-            switch(Console.ReadKey().Key){
-                case ConsoleKey.UpArrow:
-                    cursorPosition-=4;
-                    break;
-                case ConsoleKey.DownArrow:
-                    cursorPosition+=4;
-                    break;
-                case ConsoleKey.RightArrow:
-                    cursorPosition+=1;
-                    break;
-                case ConsoleKey.LeftArrow:
-                    cursorPosition-=1;
-                    break;
-                case ConsoleKey.Spacebar:
-                    switch(cursorPosition) {
-                        //First Button Row
-                        case 0:
-                            if (numberOn < 2) {
-                                result += "1";
-                                currentNumber += "1";
-                            }
-                            break;
-                        case 1:
-                            if (numberOn < 2) {
-                                result += "2";
-                                currentNumber += "2";
-                            }
-                            break;
-                        case 2:
-                            if (numberOn < 2) {
-                                result += "3";
-                                currentNumber += "3";
-                            }
-                            break;
-                        case 3:
-                            if (numberOn==0){
-                                result += "+";
-                                thing = 0;
-                                numberOn = 1;
-                                firstNumber = Convert.ToUInt32(currentNumber);
-                                currentNumber = "";
-                            } else if (numberOn==1) {
-                                thing = 0;
-                                if(result.Contains('-')) {
-                                    result = result.Replace('-', '+');
-                                } else if(result.Contains('*')) {
-                                    result = result.Replace('*', '+');
-                                } else if(result.Contains('÷')) {
-                                    result = result.Replace('÷', '+');
-                                }
-                                
-                            }
-                            break;
-                        //Second Button Row
-                        case 4:
-                            if (numberOn < 2) {
-                                result += "4";
-                                currentNumber += "4";
-                            }
-                            break;
-                        case 5:
-                            if (numberOn < 2) {
-                                result += "5";
-                                currentNumber += "5";
-                            }
-                            break;
-                        case 6:
-                            if (numberOn < 2) {
-                                result += "6";
-                                currentNumber += "6";
-                            }
-                            break;
-                        case 7:
-                            if (numberOn==0){
-                                result += "-";
-                                thing = 1;
-                                numberOn = 1;
-                                firstNumber = Convert.ToUInt32(currentNumber);
-                                currentNumber = "";
-                            } else if (numberOn==1) {
-                                thing = 1;
-                                if(result.Contains('+')) {
-                                    result = result.Replace('+', '-');
-                                } else if(result.Contains('*')) {
-                                    result = result.Replace('*', '-');
-                                } else if(result.Contains('÷')) {
-                                    result = result.Replace('÷', '-');
-                                }
-                                
-                            }
-                            break;
-                        //Third Button Row
-                        case 8:
-                            if (numberOn < 2) {
-                                result += "7";
-                                currentNumber += "7";
-                            }
-                            break;
-                        case 9:
-                            if (numberOn < 2) {
-                                result += "8";
-                                currentNumber += "8";
-                            }
-                            break;
-                        case 10:
-                            if (numberOn < 2) {
-                                result += "9";
-                                currentNumber += "9";
-                            }
-                            break;
-                        case 11:
-                            if (numberOn==0){
-                                result += "*";
-                                thing = 2;
-                                numberOn = 1;
-                                firstNumber = Convert.ToUInt32(currentNumber);
-                                currentNumber = "";
-                            } else if (numberOn==1) {
-                                thing = 2;
-                                if(result.Contains('+')) {
-                                    result = result.Replace('+', '*');
-                                } else if(result.Contains('-')) {
-                                    result = result.Replace('-', '*');
-                                } else if(result.Contains('÷')) {
-                                    result = result.Replace('÷', '*');
-                                }
-                                
-                            }
-                            break;
-                        //Forth Button Row
-                        case 12:
-                            result = string.Empty;
-                            currentNumber = string.Empty;
-                            firstNumber = 0;
-                            secondNumber = 0;
-                            thing = 0;
-                            numberOn = 0;
-                            break;
-                        case 13:
-                            if (numberOn < 2) {
-                                result += "0";
-                                currentNumber += "0";
-                            }
-                            break;
-                        case 14:
-                            if (numberOn == 1) {
-                                result += "=";
-                                numberOn = 2;
-                                secondNumber = Convert.ToUInt32(currentNumber);
-                                switch(thing) {
-                                    case 0:
-                                        result += (firstNumber + secondNumber);
-                                        break;
-                                    case 1:
-                                        result += (firstNumber - secondNumber);
-                                        break;
-                                    case 2:
-                                        result += (firstNumber * secondNumber);
-                                        break;
-                                    case 3:
-                                        result += (firstNumber / secondNumber);
-                                        break;
-                                }
-                            }
-                            break;
-                        case 15:
-                            if (numberOn==0){
-                                result += "÷";
-                                thing = 3;
-                                numberOn = 1;
-                                firstNumber = Convert.ToUInt32(currentNumber);
-                                currentNumber = "";
-                            } else if (numberOn==1) {
-                                thing = 3;
-                                if(result.Contains('+')) {
-                                    result = result.Replace('+', '÷');
-                                } else if(result.Contains('-')) {
-                                    result = result.Replace('-', '÷');
-                                } else if(result.Contains('*')) {
-                                    result = result.Replace('*', '÷');
-                                }
-                                
-                            }
-                            break;
-                    }
-                    break;
-                case ConsoleKey.Escape:
-                    running = false;
-                    break;
-                default:
-                    break;
-            }
-        }
-        
-        static void Main(string[] args) {
-            Console.WriteLine("Calculator by 7Games\n\nUse [Arrow Keys] to move cursor\nUse the [Space Bar] the select.\nPress [ESC] to exit.");
-            Console.ReadKey();
-            Console.Clear();
+							numberOn = 1;
+							firstNumber = Convert.ToUInt32(currentNumber);
+							currentNumber = "";
+						}  else if (numberOn==1) {
+								int[] operatorLocation = {3, 7, 11, 15};
+								char[] charCalculator = calculator.ToCharArray();
 
-            while(running){
-                Console.SetCursorPosition(0, 0);
-                drawCalaulator();
-                input();
-            }
-        }
-    }
+								for(int i = 0; i < 3; i++) {
+									if (cursorPosition - operatorLocation[i] == 0) {
+										calcOperator = i;
+									}
+								}
+
+								//Replaces the current operator with the one the user selects.
+								if (result.Contains('+')) 
+									result = result.Replace('+', charCalculator[buttonLocations[cursorPosition] + 1]);
+								else if (result.Contains('-')) 
+									result = result.Replace('-', charCalculator[buttonLocations[cursorPosition] + 1]);
+								else if (result.Contains('x')) 
+									result = result.Replace('x', charCalculator[buttonLocations[cursorPosition] + 1]);
+								else if (result.Contains('÷')) 
+									result = result.Replace('÷', charCalculator[buttonLocations[cursorPosition] + 1]);
+							}
+					}
+					//Clear and Equal Buttons
+					switch(cursorPosition) {
+						case 12:
+							result = string.Empty;
+							currentNumber = string.Empty;
+							firstNumber = 0;
+							secondNumber = 0;
+							calcOperator = 0;
+							numberOn = 0;
+							break;
+						case 14:
+							if (numberOn == 1) {
+								result += "=";
+								numberOn = 2;
+								secondNumber = Convert.ToUInt32(currentNumber);
+								switch(calcOperator) {
+									case 0:
+										result += (firstNumber + secondNumber);
+										break;
+									case 1:
+										result += (firstNumber - secondNumber);
+										break;
+									case 2:
+										result += (firstNumber * secondNumber);
+										break;
+									case 3:
+										result += (firstNumber / secondNumber);
+										break;
+								}
+							}
+							break;
+					}
+					break;
+				case ConsoleKey.Escape:
+					running = false;
+					break;
+				default:
+					break;
+			}
+		}
+		
+		static void Main(string[] args) {
+			Console.WriteLine("Calculator by 7Games\n\nUse [Arrow Keys] to move cursor\nUse the [Space Bar] the select.\nPress [ESC] to exit.");
+			Console.ReadKey();
+			Console.Clear();
+
+			while(running){
+				calculator = "╔═══════════╗\n║···········║\n╠══╦══╦══╦══╣\n║ 1║ 2║ 3║ +║\n╠══╬══╬══╬══╣\n║ 4║ 5║ 6║ -║\n╠══╬══╬══╬══╣\n║ 7║ 8║ 9║ x║\n╠══╬══╬══╬══╣\n║ C║ 0║ =║ ÷║\n╚══╩══╩══╩══╝";
+				Console.SetCursorPosition(0, 0);
+				drawCalaulator();
+				input();
+			}
+		}
+	}
 }
